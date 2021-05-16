3bcbfb4d69f49375e66e27603efe95baf750bed6
ZhijiZiji <cgh18401448781@163.com>
2021-05-16 19:48:32 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/guohui/git/HelloGit.java
@@ -0,0 +1,15 @@
+package com.guohui.git;
+
+/**
+ * @author 程国辉
+ * @description HelloGit
+ * @since 2021/5/16 19:33
+ */
+
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+
+}
